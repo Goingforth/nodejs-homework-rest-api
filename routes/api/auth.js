bad5f875583588d12d4ca1f3b8d77908d4348eea
Oleksandr Siukov <as-service2008@ukr.net>
2023-07-31 23:39:51 +0300

    registr + login

---
+++ routes/api/auth.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/auth");
+
+const { validateBody } = require("../../middlewares");
+
+const router = express.Router();
+
+const { schemas } = require("../../models/user");
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+module.exports = router;
