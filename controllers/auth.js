92d05af86272a3e62c080590766b596fbe18804a
Oleksandr Siukov <as-service2008@ukr.net>
2023-08-01 13:26:05 +0300

    work

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,6 +40,8 @@ const login = async (req, res) => {
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
+  await User.findByIdAndUpdate(user._id, { token });
+
   res.json({
     token,
     user: {
@@ -49,7 +51,26 @@ const login = async (req, res) => {
   });
 };
 
+const getCurrent = async (req, res) => {
+  const { email, name } = req.user;
+
+  res.json({
+    email,
+    subscription: "starter",
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+
+  // res.json({
+  //   message:
+  // })
+};
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
 };
